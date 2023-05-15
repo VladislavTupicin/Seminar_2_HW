49ec449d3dfedb5bf54d5325bc053f9d64734ff9
vladt <tupicinvlad2@gmail.com>
2023-05-15 10:52:57 +0300

    Initial Commit

---
+++ Seminar_2_HW/Task_2.java
@@ -0,0 +1,16 @@
+public class Task_2 {
+    public static void main(String[] args) {
+        correctCode2();
+    }
+
+    private static void correctCode2() {
+        try {
+            int[] intArray = { 2, 3, 4, 5, 6, 7 };
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
